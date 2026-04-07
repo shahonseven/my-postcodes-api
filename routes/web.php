90d0786e58fd13445d8cae9377e7ec7bd689a7c5
Shahril Abdullah <shahonseven@gmail.com>
2026-04-07 11:38:57 +0000

    refactor: move API routes to routes/api.php
    - Move all API endpoints from web.php to api.php
    - Clean up web.php to only contain web routes
    - API routes now use Laravel's built-in api.php routing file

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\PostcodeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-// Public auth routes
-Route::prefix('api')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-});
-
-// Protected API routes
-Route::prefix('api')->middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/tokens', [AuthController::class, 'tokens']);
-    Route::delete('/tokens/{tokenId}', [AuthController::class, 'revokeToken']);
-    Route::delete('/tokens', [AuthController::class, 'revokeAllTokens']);
-
-    // Postcode endpoints
-    Route::get('/states', [PostcodeController::class, 'states']);
-    Route::get('/cities', [PostcodeController::class, 'cities']);
-    Route::get('/postcode/{postcode}', [PostcodeController::class, 'lookup']);
-    Route::get('/search', [PostcodeController::class, 'search']);
 });
